ba606614522d2026a35e8e5e25670352fb9a991f
Shawon <mdmoinulhossainshawon@gmail.com>
2024-12-06 11:50:51 +0600

    fix: Changed how redrawing works
    Ref: #216

--- plugin/markview.lua
+++ plugin/markview.lua
@@ -1,4 +1,6 @@
 local markview = require("markview");
+local spec = require("markview.spec");
+
 require("markview.highlights").setup();
 
 --- Patch for the broken (fenced_code_block) concealment
@@ -26,37 +28,43 @@ vim.treesitter.query.add_directive("conceal-patch!", function (match, _, bufnr, 
 	metadata[id].conceal = "";
 end)
 
---- Autocmd for attaching to a buffer
+--- Autocmd for attaching to a buffer.
 vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter" }, {
 	group = markview.augroup,
 	callback = function (event)
-		local spec = require("markview.spec");
 		local buffer = event.buf or vim.api.nvim_get_current_buf();
 		local bt, ft = vim.bo[buffer].buftype, vim.bo[buffer].filetype;
 
-		if
-			spec.get({ "enable" }) ~= false and
-			vim.list_contains(
-				spec.get({ "preview", "filetypes" }, { fallback = {}, ignore_enable = true }),
-				ft
-			) and
-			not vim.list_contains(
-				spec.get({ "preview", "ignore_buftypes" }, { fallback = {}, ignore_enable = true }),
-				bt
-			)
-		then
-			-- vim.uv.new_timer():start(0, 0, vim.schedule_wrap(function ()
-				markview.commands.attach(event.buf);
-			-- end));
+		local attach_ft = spec.get({ "preview", "filetypes" }, { fallback = {}, ignore_enable = true });
+		local ignore_bt = spec.get({ "preview", "filetypes" }, { fallback = {}, ignore_enable = true });
+
+		--- Check if it's possible to attach to
+		--- the buffer.
+		---@return boolean
+		local can_attach = function ()
+			if spec.get({ "enable" }) == false or markview.state.enable == false then
+				--- Plugin is disabled.
+				return false;
+			elseif vim.list_contains(attach_ft, ft) == false  then
+				return false;
+			elseif vim.list_contains(ignore_bt, bt) == true then
+				return false;
+			end
+
+			return true;
+		end
+
+		if can_attach() == true then
+			markview.commands.attach(event.buf);
 		end
 	end
 });
 
+--- Autocmd to listen to mode changes.
 vim.api.nvim_create_autocmd({ "ModeChanged" }, {
 	group = markview.group,
 	callback = function ()
 		local renderer = require("markview.renderer");
-		local spec = require("markview.spec");
 
 		local preview_modes = spec.get({ "preview", "modes" }, { fallback = {}, ignore_enable = true });
 		local mode = vim.api.nvim_get_mode().mode;
@@ -94,6 +102,63 @@ vim.api.nvim_create_autocmd({ "ModeChanged" }, {
 			::continue::
 			---_
 		end
+	end
+});
+
+local events = spec.get({ "preview", "redraw_events" }, { fallback = {}, ignore_enable = true });
+local preview_modes = spec.get({ "preview", "modes" }, { fallback = {}, ignore_enable = true });
+
+if
+	vim.list_contains(preview_modes, "n") or
+	vim.list_contains(preview_modes, "v")
+then
+	table.insert(events, "CursorMoved");
+	table.insert(events, "TextChanged");
+end
+
+if vim.list_contains(preview_modes, "i") then
+	table.insert(events, "CursorMovedI");
+	table.insert(events, "TextChangedI");
+end
+
+local debounce_delay = spec.get({ "preview", "debounce" }, { fallback = 50, ignore_enable = true });
+local debounce = vim.uv.new_timer();
+
+vim.api.nvim_create_autocmd(events, {
+	group = markview.group,
+	callback = function (param)
+		--- Checks if decorations can be drawn on a buffer.
+		---@param buffer integer
+		---@return boolean
+		local can_draw = function (buffer)
+			---+${func}
+			markview.clean();
+
+			if not buf_is_safe(buffer) then
+				return false;
+			elseif markview.state.enable == false then
+				return false;
+			elseif markview.state.buffer_states[buffer] == false then
+				return false;
+			end
+
+			return true;
+			---_
+		end
+
+		local attached_bufs = vim.tbl_keys(markview.state.buffer_states);
+		debounce:stop();
+
+		if vim.list_contains(attached_bufs, param.buf) == false then
+			return;
+		end
+
+		debounce:start(debounce_delay, 0, vim.schedule_wrap(function ()
+			--- Drawer function
+			if can_draw(param.buf) == false then return; end
+			markview.draw(param.buf);
+		end));
+
 	end
 });
 
