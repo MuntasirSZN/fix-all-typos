4e77f7def4d7c9296a3296f712efc156d28539f6
Shawon <mdmoinulhossainshawon@gmail.com>
2024-12-30 15:10:21 +0600

    doc: Added type definitions for inline markdown.
    Ref: #177
    
    feat: Added highlight group for file names & reference IDs in block
    reference links.
    
    Ref: #177

--- lua/markview/parsers/markdown_inline.lua
+++ lua/markview/parsers/markdown_inline.lua
@@ -18,6 +18,7 @@ inline.insert = function (data)
 end
 
 --- Cached stuff
+---@type { checkbox: { [integer]: table } }
 inline.cache = {
 	checkbox = {},
 	link_ref = {}
@@ -25,126 +26,166 @@ inline.cache = {
 
 --- Checkbox parser.
 ---@param buffer integer
----@param range TSNode.range
-inline.checkbox = function (buffer, _, _, range)
+---@param range node.range
+inline.checkbox = function (buffer, _, text, range)
+	---+${lua}
+
 	local line = vim.api.nvim_buf_get_lines(buffer, range.row_start, range.row_start + 1, false)[1];
 
 	local before = line:sub(0, range.col_start);
 	local inner = line:sub(range.col_start + 1, range.col_end - 1);
 
-	if not (before:match("^[%s%>]*[%-%+%*]%s$") or before:match("^[%s%>]*%d+[%.%)]%s$")) then
+	if before:match("^[%s%>]*[%-%+%*]%s$") == nil and before:match("^[%s%>]*%d+[%.%)]%s$") == nil then
 		return;
 	end
 
+	---@type __inline.checkboxes
 	inline.insert({
 		class = "inline_checkbox",
+		state = inner:gsub("[%[%]]", ""),
 
-		text = inner:gsub("[%[%]]", ""),
-
+		text = text,
 		range = range
 	});
 
-	inline.cache.checkbox[range.row_start] = #inline.content;
+	--- Cache the current checkbox.
+	--- TODO, Has no use *yet*.
+	inline.cache.checkbox[range.row_start] = inline.content[#inline.content];
+	---_
 end
 
 --- Inline code parser.
 ---@param text string[]
----@param range TSNode.range
+---@param range node.range
 inline.code_span = function (_, _, text, range)
+	---+${lua}
+
+	---@type __inline.inline_codes
 	inline.insert({
 		class = "inline_code_span",
 
 		text = text,
 		range = range
-	})
+	});
+	---_
 end
 
 --- Embed file link parser.
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.embed_file = function (_, _, text, range)
-	local class = "inline_link_embed_file";
-	local tmp, label;
-
+	---+${lua}
 	if text[1]:match("%#%^(.+)%]%]$") then
-		class = "inline_link_block_ref";
-		tmp, label = text[1]:match("^(.*)%#%^(.+)%]%]$");
+		local file, block = text[1]:match("^%!%[%[(.*)%#%^(.+)%]%]$");
+		range.label = { range.row_start, range.col_start + 3, range.row_end, range.col_end - 2 };
 
-		range.label_start = range.col_start + #tmp + 2;
-		range.label_end = range.col_end - 2;
+		range.block = { range.row_start, range.col_start + 3 + #file + 2, range.row_end, range.col_end - 2 };
+
+		if file ~= "" then
+			range.file  = { range.row_start, range.col_start + 3, range.row_end, range.col_start + 3 + #file };
+		else
+			file = nil;
+		end
+
+		---@type __inline.embed_files
+		inline.insert({
+			class = "inline_link_block_ref",
+			file = file,
+			block = block,
+			label = string.format("%s#^%s", file, block),
+
+			text = text,
+			range = range
+		});
 	else
-		label = text[1]:match("%[%[([^%[+])%]%]");
+		---@type __inline.embed_files
+		inline.insert({
+			class = "inline_link_embed_file",
+			label = text[1]:match("%[%[([^%[+])%]%]"),
+
+			text = text,
+			range = range
+		});
 	end
-
-	inline.insert({
-		class = class,
-		has_file = class == "inline_link_block_ref",
-
-		text = text,
-		label = label,
-
-		range = range
-	});
+	---_
 end
 
 --- Email parser.
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.email = function (_, _, text, range)
-	range.label_start = range.col_start + 1;
-	range.label_end = range.col_end - 1;
+	---+${lua}
 
+	range.label = { range.row_start, range.col_start + 1, range.row_end, range.col_end - 1 };
+
+	---@type __inline.emails
 	inline.insert({
 		class = "inline_link_email",
-
-		text = text[1]:sub(range.col_start, range.col_end),
 		label = text[1]:sub(range.col_start + 2, range.col_end - 1),
 
+		text = text,
 		range = range
-	})
+	});
+	---_
 end
 
 --- Uri autolink parser.
 ---@param text string[]
----@param range TSNode.range
+---@param range node.range
 inline.entity = function (_, _, text, range)
+	---+${lua}
+
+	---@type __inline.entities
 	inline.insert({
 		class = "inline_entity",
+		name = text[1]:gsub("[^%a%d]", ""),
 
-		text = text[1]:gsub("[^%a%d]", ""),
+		text = text,
 		range = range
-	})
+	});
+	---_
 end
 
 --- Uri autolink parser.
 ---@param text string[]
+---@param range node.range
 inline.escaped = function (_, _, text, range)
+	---+${lua}
+
+	---@type __inline.escaped
 	inline.insert({
 		class = "inline_escaped",
 
 		text = text[1]:sub(range.col_start + 1, range.col_end - 1),
 		range = range
 	});
+	---_
 end
 
 --- Footnote parser.
 ---@param text string[]
----@param range __inline.link_range
+---@param range node.range
 inline.footnote = function (_, _, text, range)
+	---+${lua}
+
+	---@type __inline.footnotes
 	inline.insert({
 		class = "inline_footnote",
 
-		text = text[1]:sub(range.col_start + 1, range.col_end - 1),
+		text = text[1],
 		label = text[1]:sub(range.col_start + 2, range.col_end - 1),
 
 		range = range
 	});
+	---_
 end
 
 --- Highlight parser.
 ---@param buffer integer
----@param range TSNode.range
+---@param range node.range
 inline.highlights = function (buffer, _, _, range)
+	---+${lua}
+
 	local utils = require("markview.utils");
 	local lines = vim.api.nvim_buf_get_lines(buffer, range.row_start, range.row_end + (range.row_end == range.row_start and 1 or 0), false);
 
@@ -154,6 +195,7 @@ inline.highlights = function (buffer, _, _, range)
 		for highlight in line:gmatch("%=%=([^=]+)%=%=") do
 			local c_s, c_e = _line:find("%=%=" .. utils.escape_string(highlight) .. "%=%=")
 
+			---@type __inline.highlights
 			inline.insert({
 				class = "inline_highlight",
 				text = highlight,
@@ -172,31 +214,40 @@ inline.highlights = function (buffer, _, _, range)
 			end, 1)
 		end
 	end
+	---_
 end
 
 --- Image link parser.
 ---@param TSNode table
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.image = function (buffer, TSNode, text, range)
+	---+${lua}
 	if text[1]:match("^%!%[%[") and text[1]:match("%]%]$") then
+		--- This is an embed file,
+		--- Not an image!
 		inline.embed_file(buffer, TSNode, text, range);
 		return;
 	end
 
+	---@cast range inline_link.range
+
 	local link_label;
 	local link_desc;
 
-	if TSNode:named_child(0) then
-		link_label = vim.treesitter.get_node_text(TSNode:named_child(0), buffer):gsub("[%[%]%(%)]", "");
-		range.label = { TSNode:named_child(0):range() };
+	for child in TSNode:iter_children() do
+		if child:type() == "image_description" then
+			link_label = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.label = { child:range() };
+		elseif child:type() == "link_label" or child:type() == "link_destination" then
+			link_desc = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.description = { child:range() };
+		end
 	end
 
-	if TSNode:named_child(1) then
-		link_desc = vim.treesitter.get_node_text(TSNode:named_child(1), buffer):gsub("[%[%]%(%)]", "");
-		range.description = { TSNode:named_child(1):range() };
-	end
+	range.label = range.label or { range.row_start, range.col_start + 2, range.row_end, range.col_start - 3 };
 
+	---@type __inline.images
 	inline.insert({
 		class = "inline_link_image",
 
@@ -205,27 +256,33 @@ inline.image = function (buffer, TSNode, text, range)
 		label = link_label,
 
 		range = range
-	})
+	});
+	---_
 end
 
 --- Hyperlink parser.
 ---@param buffer integer
 ---@param text string[]
----@param range __inline.link_range
-inline.inline_link = function (buffer, TSNode, text, range)
+---@param range inline_link.range
+inline.inline_hyperlink = function (buffer, TSNode, text, range)
+	---+${lua}
+
 	local link_desc;
 	local link_label;
 
-	if TSNode:named_child(0) then
-		link_label = vim.treesitter.get_node_text(TSNode:named_child(0), buffer):gsub("[%[%]]", "");
-		range.label = { TSNode:named_child(0):range() };
+	for child in TSNode:iter_children() do
+		if child:type() == "link_text" then
+			link_label = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.label = { child:range() };
+		elseif child:type() == "link_label" or child:type() == "link_destination" then
+			link_desc = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.description = { child:range() };
+		end
 	end
 
-	if TSNode:named_child(1) then
-		link_desc = vim.treesitter.get_node_text(TSNode:named_child(1), buffer):gsub("[%[%]%(%)]", "");
-		range.description = { TSNode:named_child(1):range() };
-	end
+	range.label = range.label or { range.row_start, range.col_start + 1, range.row_end, range.col_start - 1 };
 
+	---@type __inline.hyperlinks
 	inline.insert({
 		class = "inline_link_hyperlink",
 
@@ -235,63 +292,84 @@ inline.inline_link = function (buffer, TSNode, text, range)
 
 		range = range
 	});
+	---_
 end
 
 --- Uri autolink parser.
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.internal_link = function (_, _, text, range)
-	local class, alias = "inline_link_internal", nil;
-	local label;
+	---+${lua}
+	if string.match(text[1], "%#%^.+$") ~= nil then
+		--- Embed files
+		local file, block = text[1]:match("^%[%[(.*)%#%^(.+)$");
+		range.label = { range.row_start, range.col_start + 2, range.row_end, range.col_end - 2 };
 
-	---@diagnostic disable-next-line
-	text = text[1]:gsub("[%[%]]", "");
+		range.block = { range.row_start, range.col_start + 2 + #file + 2, range.row_end, range.col_end - 2 };
 
-	if text:match("%#%^(.+)$") then
-		local tmp;
+		if file ~= "" then
+			range.file  = { range.row_start, range.col_start + 2, range.row_end, range.col_start + 2 + #file };
+		else
+			file = nil;
+		end
 
-		class = "inline_link_block_ref";
-		tmp, label = text:match("^(.*)%#%^(.+)$");
+		---@type __inline.block_references
+		inline.insert({
+			class = "inline_link_block_ref",
+			file = file,
+			block = block,
+			label = string.format("%s#^%s", file, block),
 
-		range.label_start = range.col_start + #tmp + 2;
-		range.label_end = range.col_end - 2;
-	elseif text:match("%|([^%|]+)$") then
-		label = text;
-		range.alias_start, range.alias_end, alias = text:find("%|([^%|]+)$");
+			text = text,
 
-		range.alias_start = range.alias_start + range.col_start + 2;
-		range.alias_end = range.alias_end + range.col_start + 2;
+			range = range
+		});
+	else
+		local label = text[1]:match("^%[%[(.+)%]%]");
+		local alias_start, alias_end, alias = text[1]:find("%|([^%|]+)%]%]$");
+
+		if type(alias) == "string" and type(alias_start) == "number" and type(alias_end) == "number" then
+			range.alias = { range.row_start, range.col_start + alias_start, range.row_end, range.col_end - 1 };
+		end
+
+		range.label = { range.row_start, range.col_start + 2, range.row_end, alias_start };
+
+		---@type __inline.internal_links
+		inline.insert({
+			class = "inline_link_internal",
+
+			text = text,
+			alias = alias,
+			label = label,
+
+			range = range
+		});
 	end
-
-	inline.insert({
-		class = class,
-
-		text = text,
-		alias = alias,
-		label = label,
-
-		range = range
-	});
+	---_
 end
 
 --- Reference link parser.
 ---@param buffer integer
 ---@param TSNode table
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.reference_link = function (buffer, TSNode, text, range)
+	---+${lua}
+
 	local link_desc;
 	local link_label;
 
-	if TSNode:named_child(0) then
-		link_label = vim.treesitter.get_node_text(TSNode:named_child(0), buffer):gsub("[%[%]]", "");
-		range.label = { TSNode:named_child(0):range() };
+	for child in TSNode:iter_children() do
+		if child:type() == "image_description" then
+			link_label = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.label = { child:range() };
+		elseif child:type() == "link_label" or child:type() == "link_destination" then
+			link_desc = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.description = { child:range() };
+		end
 	end
 
-	if TSNode:named_child(1) then
-		link_desc = vim.treesitter.get_node_text(TSNode:named_child(1), buffer):gsub("[%[%]%(%)]", "");
-		range.description = { TSNode:named_child(1):range() };
-	end
+	range.label = range.label or { range.row_start, range.col_start + 2, range.row_end, range.col_start - 3 };
 
 	inline.insert({
 		class = "inline_link_hyperlink",
@@ -302,14 +380,17 @@ inline.reference_link = function (buffer, TSNode, text, range)
 
 		range = range
 	});
+	---_
 end
 
 --- Shortcut link parser.
 ---@param buffer integer
 ---@param TSNode table
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.shortcut_link = function (buffer, TSNode, text, range)
+	---+${lua}
+
 	local s_line = vim.api.nvim_buf_get_lines(buffer, range.row_start, range.row_start + 1, false)[1];
 	local e_line = vim.api.nvim_buf_get_lines(buffer, range.row_end, range.row_end + 1, false)[1];
 
@@ -349,38 +430,40 @@ inline.shortcut_link = function (buffer, TSNode, text, range)
 
 	::invalid_link::
 
-	local label = "";
+	local link_desc;
+	local link_label;
 
-	for l, line in ipairs(text) do
-		if l == 1 then
-			line = line:gsub("^%[", "");
-		elseif l == #text then
-			line = line:gsub("%]$", "");
+	for child in TSNode:iter_children() do
+		if child:type() == "link_text" then
+			link_label = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.label = { child:range() };
+		elseif child:type() == "link_label" or child:type() == "link_destination" then
+			link_desc = vim.treesitter.get_node_text(child, buffer):gsub("[%[%]%(%)]", "");
+			range.description = { child:range() };
 		end
-
-		if label ~= "" then
-			label = label .. "\n";
-		end
-
-		label = label .. line;
 	end
+
+	range.label = range.label or { range.row_start, range.col_start + 1, range.row_end, range.col_start - 1 };
 
 	inline.insert({
 		class = "inline_link_shortcut",
+		label = link_label,
+		description = link_desc,
 
 		text = text,
-		label = label,
-
 		range = range
-	})
+	});
+	---_
 end
 
 --- Uri autolink parser.
 ---@param TSNode table
 ---@param text string[]
----@param range __inline.link_range
+---@param range inline_link.range
 inline.uri_autolink = function (_, TSNode, text, range)
-	range.label = { range.row_start, range.col_start, range.row_end, range.col_end };
+	---+${lua}
+
+	range.label = { range.row_start, range.col_start + 1, range.row_end, range.col_end - 1 };
 
 	inline.insert({
 		class = "inline_link_uri_autolink",
@@ -390,10 +473,20 @@ inline.uri_autolink = function (_, TSNode, text, range)
 		label = text[1]:sub(range.col_start + 1, range.col_end - 1),
 
 		range = range
-	})
+	});
+	---_
 end
 
+--- Inline markdown parser.
+---@param buffer integer
+---@param TSTree table
+---@param from integer?
+---@param to integer?
+---@return table[]
+---@return table
 inline.parse = function (buffer, TSTree, from, to)
+	---+${lua}
+
 	inline.sorted = {};
 	inline.content = {};
 
@@ -428,7 +521,7 @@ inline.parse = function (buffer, TSTree, from, to)
 
 		([
 			(inline_link)
-			(collapsed_reference_link)] @markdown_inline.inline_link)
+			(collapsed_reference_link)] @markdown_inline.inline_hyperlink)
 
 		((full_reference_link
 			(link_text)
@@ -491,6 +584,7 @@ inline.parse = function (buffer, TSTree, from, to)
 	end
 
 	return inline.content, inline.sorted;
+	---_
 end
 
 return inline;
