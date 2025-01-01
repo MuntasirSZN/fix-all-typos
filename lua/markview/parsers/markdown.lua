MuntasirSZN/fix-all-typos

commit d1f40eb618bd09828a7e761af8d29c6aa23bfa53

fix: Fixed a bug causing errors when exiting Neovim
API functions are no longer used on the main parser function.

Ref: #177

lua/markview/parsers/markdown.lua
@@ -552,17 +552,27 @@ markdown.parse = function (buffer, TSTree, from, to)
 
 	for capture_id, capture_node, _, _ in scanned_queries:iter_captures(TSTree:root(), buffer, from, to) do
 		local capture_name = scanned_queries.captures[capture_id];
+
+		if not capture_name:match("^markdown%.") then
+			goto continue;
+		end
+
+		---@type string?
+		local capture_text = vim.treesitter.get_node_text(capture_node, buffer);
 		local r_start, c_start, r_end, c_end = capture_node:range();
 
-		local capture_text = vim.api.nvim_buf_get_lines(buffer, r_start, r_start == r_end and r_end + 1 or r_end, false);
+		if capture_text == nil then
+			goto continue;
+		end
 
-		if capture_name ~= "markdown.list_item" and capture_name ~= "markdown.checkbox" then
-			local spaces = capture_text[1]:sub(c_start + 1):match("^(%s*)");
-			c_start = c_start + #spaces;
+		if not capture_text:match("\n$") then
+			capture_text = capture_text .. "\n";
+		end
 
-			for l, line in ipairs(capture_text) do
-				capture_text[l] = line:sub(c_start + 1)
-			end
+		local lines = {};
+
+		for line in capture_text:gmatch("(.-)\n") do
+			table.insert(lines, line);
 		end
 
 		pcall(
@@ -570,7 +580,7 @@ markdown.parse = function (buffer, TSTree, from, to)
 
 			buffer,
 			capture_node,
-			capture_text,
+			lines,
 			{
 				row_start = r_start,
 				col_start = c_start,
@@ -579,6 +589,8 @@ markdown.parse = function (buffer, TSTree, from, to)
 				col_end = c_end
 			}
 		);
+
+		::continue::
 	end
 
 	return markdown.content, markdown.sorted;

commit 8fb75014acd6d10f39e914212e67f81aea3a3dda

fix: Fixes nested code block parsing
Fixes a bug introduced in d1f40eb618bd09828a7e761af8d29c6aa23bfa53.

Ref: #177

lua/markview/parsers/markdown.lua
@@ -123,10 +123,20 @@ markdown.checkbox = function (_, _, text, range)
 end
 
 --- Code block parser
----@param text string[]
 ---@param range __code_blocks.range
-markdown.code_block = function (_, _, text, range)
+markdown.code_block = function (buffer, _, _, range)
 	---+${lua}
+
+	--- Parser is unreliable.
+	--- Use buffer lines.
+	---@type string[]
+	local text = vim.api.nvim_buf_get_lines(buffer, range.row_start, range.row_end, false);
+
+	--- Modify the text so that only the text
+	--- inside the node's range is visible.
+	for l, line in ipairs(text) do
+		text[l] = line:sub(range.col_start + 1);
+	end
 
 	local tmp, before = text[1], nil;
 	local language, info;