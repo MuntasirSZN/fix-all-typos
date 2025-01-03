cd88dd98a80cc685a5f678b65eb37e2839e61387
Shawon <mdmoinulhossainshawon@gmail.com>
2025-01-03 21:01:00 +0600

    feat: More control over what gets concealed
    Closes #220

--- lua/markview/renderer.lua
+++ lua/markview/renderer.lua
@@ -9,9 +9,261 @@ renderer.typst = require("markview.renderers.typst");
 
 renderer.cache = {};
 
+renderer.__filter_cache = {
+	config = nil,
+	result = nil
+};
+
+--- Maps a `class` to an option name.
+renderer.opt_map = {
+	---+${lua}
+
+	html = {
+		html_container_element = "container_elements",
+		html_heading = "headings",
+		html_void_element = "void_elements"
+	},
+	latex = {
+		latex_block = function (item)
+			return item.inline == true and "inlines" or "blocks";
+		end,
+		latex_command = "commands",
+		latex_escaped = "escapes",
+		latex_font = "fonts",
+		latex_inline = "inlines",
+		latex_parenthesis = "parenthesis",
+		latex_subscripts = "subscripts",
+		latex_superscript = "superscripts",
+		latex_symbol = "symbols",
+		latex_text = "texts"
+	},
+	markdown = {
+		markdown_atx_heading = "headings",
+		markdown_block_quote = "block_quotes",
+		markdown_code_block = "code_blocks",
+		markdown_hr = "horizontal_rules",
+		markdown_link_ref_definition = "reference_definitions",
+		markdown_list_item = "list_items",
+		markdown_metadata_minus = "metadata_minus",
+		markdown_metadata_plus = "metadata_plus",
+		markdown_setext_heading = "headings",
+		markdown_table = "tables"
+	},
+	markdown_inline = {
+		inline_checkbox = "checkboxes",
+		inline_code_span = "inline_codes",
+		inline_entity = "entities",
+		inline_escaped = "escapes",
+		inline_footnote = "footnotes",
+		inline_highlight = "highlights",
+		inline_link_block_ref = "block_references",
+		inline_link_embed_file = "embed_files",
+		inline_link_email = "emails",
+		inline_link_hyperlink = "hyperlinks",
+		inline_link_image = "images",
+		inline_link_shortcut = "hyperlinks",
+		inline_link_uri_autolink = "uri_autolinks",
+		inline_link_internal = "internal_links"
+	},
+	typst = {
+		typst_code_block = "code_blocks",
+		typst_code_inline = "code_inlines",
+		typst_escaped = "escapes",
+		typst_heading = "headings",
+		typst_label = "labels",
+		typst_list_item = "list_items",
+		typst_link_ref = "reference_links",
+		typst_link_url = "url_links",
+		typst_math = function (item)
+			return item.inline == true and "math_inlines" or "math_blocks";
+		end,
+		typst_raw_block = "raw_blocks",
+		typst_raw_span = "raw_spans",
+		typst_subscript = "subscripts",
+		typst_superscript = "superscripts",
+		typst_symbol = "symbols",
+		typst_term = "terms"
+	},
+	yaml = {
+		yaml_property = "properties",
+	}
+	---_
+};
+
+--- Creates node class filters for hybrid mode.
+---@param filter table?
+---@return table
+local create_filter = function (filter)
+	---+${lua}
+
+	--- Reverse mapping function.
+	---@param tbl { [string | integer]: any }
+	---@param value any
+	---@return ( string | integer )[]
+	local function rmap (tbl, value)
+		---+${lua}
+
+		local _k = {};
+
+		for key, val in pairs(tbl) do
+			if val == value then
+				table.insert(_k, key);
+			end
+		end
+
+		return _k;
+		---_
+	end
+
+	--- Returns items from {source} that aren't
+	--- in {collected}.
+	---@param source any[]
+	---@param collected any[]
+	---@return any[]
+	local get_excluded = function (source, collected)
+		---+${lua}
+
+		local _t = {};
+
+		for _, item in ipairs(source or {}) do
+			if vim.list_contains(collected, item) == false then
+				table.insert(_t, item);
+			end
+		end
+
+		return _t;
+		---_
+	end
+
+	local spec = require("markview.spec");
+	local filters = filter or spec.get({ "preview", "ignore_previews" }, { fallback = {} });
+
+	if renderer.__filter_cache.result ~= nil and vim.deep_equal(renderer.__filter_cache.config, filters) == true then
+		return renderer.__filter_cache.result;
+	else
+		renderer.__filter_cache.config = filters;
+	end
+
+	--- Resulting filter.
+	local _o = {};
+
+	for lang, maps in pairs(renderer.opt_map) do
+		if vim.islist(filters[lang]) == false then
+			-- Filter doesn't exist. Add every
+			-- node class.
+			_o[lang] = vim.tbl_keys(maps);
+			goto continue;
+		end
+
+		--- Temporarily store the option maps.
+		--- Used for `!*` operator.
+		local tmp = vim.deepcopy(maps);
+
+		--- We need a table to insert entries.
+		_o[lang] = {};
+
+		--- Iterate over the filter items and
+		--- create a list of class names.
+		for _, item in ipairs(filters[lang]) do
+			---+${lua}
+			if item:match("^%!") then
+				-- Only add nodes that aren't mapped to this item!
+				local exclude = get_excluded( vim.tbl_keys(tmp), rmap(tmp, item:sub(2)) );
+
+				for _, match in ipairs(exclude) do
+					if vim.list_contains(_o[lang], match) == false then
+						table.insert(_o[lang], match);
+						tmp[match] = nil;
+					end
+				end
+			else
+				local matches = rmap(tmp, item);
+
+				for _, match in ipairs(matches) do
+					if vim.list_contains(_o[lang], match) == false then
+						--- Only add ones we haven't added before.
+						table.insert(_o[lang], match);
+						tmp[match] = nil;
+					end
+				end
+			end
+			---_
+		end
+
+	    ::continue::
+	end
+
+	renderer.__filter_cache.result = _o;
+	return _o;
+	---_
+end
+
 --- Range modifiers for various nodes.
----@type { [string]: fun(range: table): table }
-renderer.range_modifiers = {};
+---@type { [string]: fun(range: node.range): node.range }
+renderer.range_modifiers = {
+	---+${lua}
+	markdown_atx_heading = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_setext_heading = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_code_block = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_block_quote = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_hr = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_list_item = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_metadata_minus = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end,
+	markdown_metadata_plus = function (range)
+		local _r = vim.deepcopy(range)
+		_r.row_end = _r.row_end - 1;
+
+		return _r;
+	end
+	---_
+};
+
+--- Fixes node ranges for `hybrid mode`.
+---@param class string
+---@param range node.range
+---@return node.range
+renderer.fix_range = function (class, range)
+	if renderer.range_modifiers[class] == nil then
+		return range;
+	end
+
+	return renderer.range_modifiers[class](range);
+end
 
 --- Filters provided content.
 --- [Used for hybrid mode]
@@ -20,104 +272,102 @@ renderer.range_modifiers = {};
 ---@param clear [ integer, integer ]
 ---@return table
 renderer.filter = function (content, filter, clear)
-	local spec = require("markview.spec");
-	filter = filter or spec.get({ "preview", "ignore_previews" }, { fallback = {} });
+	---+${lua}
 
-	if not clear then
-		--- No clear region.
-		return content;
-	end
-
-	---@type integer?, integer?
-	local cl_from, cl_to;
-
-	--- Checks if a range contains the clear range.
-	---@param range { row_start: integer, row_end: integer }
+	--- Checks if {pos} is inside of {range}.
+	---@param range node.range
+	---@param pos [ integer, integer ]
 	---@return boolean
-	local function in_range(range)
-		if vim.islist(clear) == false then
-			--- Not within range
+	local within = function (range, pos)
+		---+${lua}
+		if type(range) ~= "table" then
 			return false;
-		elseif clear[1] >= range.row_start and clear[2] <= range.row_end then
+		elseif type(range.row_start) ~= "number" or type(range.row_end) ~= "number" then
+			return false;
+		elseif vim.islist(pos) == false then
+			return false;
+		elseif type(pos[1]) ~= "number" or type(pos[2]) ~= "number" then
+			return false;
+		elseif pos[1] >= range.row_start and pos[2] <= range.row_end then
 			return true;
-		else
-			return false;
 		end
+
+		return false;
+		---_
 	end
 
-	--- Updates the final clearing range.
-	---@param range { row_start: integer, row_end: integer }
-	local function update_range(range)
-		if type(cl_from) ~= "number" then
-			cl_from = range.row_start;
-		elseif range.row_start < cl_from then
-			cl_from = range.row_start;
-		end
+	---@type [ integer, integer ] Range to clear.
+	local clear_range = vim.deepcopy(clear);
 
-		if type(cl_to) ~= "number" then
-			cl_to = range.row_end;
-		elseif range.row_start < cl_to then
-			cl_to = range.row_end;
+	--- Updates the range to clear.
+	---@param new [ integer, integer ]
+	local range_update = function (new)
+		---+${lua}
+		if new[1] <= clear_range[1] and new[2] >= clear_range[2] then
+			clear_range[1] = new[1];
+			clear_range[2] = new[2];
 		end
+		---_
 	end
 
+	--- Node filters.
+	---@type preview.ignore
+	local result_filters = create_filter(filter);
+
+	---@type { [string]: table }
+	local indexes = {};
+
+	--- Create a range to clear.
 	for lang, items in pairs(content) do
-		local lang_filter = spec.get({ lang }, { source = filter });
+		---+${lua}
 
-		for _, item in ipairs(items) do
-			local class = item.class;
-			local range = item.range;
+		--- Filter for this language.
+		---@type string[]?
+		local lang_filter = result_filters[lang];
 
-			if renderer.range_modifiers[class] then
-				range = renderer.range_modifiers[class](range);
-			end
+		if lang_filter == nil then
+			goto continue;
+		end
 
-			class = class:gsub("^" .. lang, "");
+		indexes[lang] = {};
 
-			if vim.tbl_islist(lang_filter) == false and in_range(range) == true then
-				update_range(range);
-			elseif (inverse == true and vim.list_contains(lang_filter, item.class)) and in_range(range) == true then
-				update_range(range);
-			elseif vim.list_contains(lang_filter, item.class) == false and in_range(range) == true then
-				update_range(range);
+		for n, node in ipairs(items) do
+			if vim.list_contains(lang_filter, node.class) then
+				local range = renderer.fix_range(node.class, node.range);
+				table.insert(indexes[lang], { n, range, node.class });
+
+				if within(node.range, clear_range) == true then
+					range_update({ range.row_start, range.row_end });
+				end
 			end
 		end
+
+		::continue::
+		---_
 	end
 
-	local filtered = {};
+	--- Remove the nodes inside the `clear_range`.
+	for lang, references in pairs(indexes) do
+		---+${lua}
 
-	for lang, items in pairs(content) do
-		local lang_filter = spec.get({ lang }, { source = filter });
+		--- Amount of nodes removed in this language.
+		--- Used for offsetting the index for later nodes.
+		local removed = 0;
 
-		if filtered[lang] == nil then
-			filtered[lang] = {};
-		end
+		for _, ref in ipairs(references) do
+			local range = ref[2];
+			-- vim.print(range.row_start .. ":" .. range.row_end)
 
-		for _, item in ipairs(items) do
-			local class = item.class;
-			local range = item.range;
-
-			if renderer.range_modifiers[class] then
-				range = renderer.range_modifiers[class](range);
+			if range.row_start >= clear_range[1] and range.row_end <= clear_range[2] then
+				table.remove(content[lang], ref[1] - removed);
+				removed = removed + 1;
 			end
-
-			class = class:gsub("^" .. lang, "");
-
-			if vim.tbl_islist(lang_filter) == false and in_range(range) == true then
-				goto skip_item;
-			elseif (inverse == true and vim.list_contains(lang_filter, item.class)) and in_range(range) == true then
-				goto skip_item;
-			elseif vim.list_contains(lang_filter, item.class) == false and in_range(range) == true then
-				goto skip_item;
-			end
-
-			table.insert(filtered[lang], item);
-
-			::skip_item::
 		end
+		---_
 	end
 
-	return filtered;
+	return content;
+	---_
 end
 
 --- Renders things
